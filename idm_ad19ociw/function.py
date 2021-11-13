1567f6fc50334f001064b9d42edd98f640bb3653
BaterrazZ <Paul-zech@outlook.com>
2021-11-13 13:46:43 +0100

    korrektur

--- idm_ad19ociw/function.py
+++ idm_ad19ociw/function.py
@@ -12,7 +12,7 @@ def imshow(X, resize=None):
     # display(Im)
     # resize
 
-    Img_1 = Image.fromarray(np.uint8(cm.gist_earth(X) * 255))  # bring maximum value to 255, convert to pillow Image
+    Img_1 = Image.fromarray(X)  # bring maximum value to 255, convert to pillow Image
     Img_1 = Img_1.resize(resize)
     display(Img_1)
     # interact(plot_Im(Img_1), resize = (100, 200,1000))
